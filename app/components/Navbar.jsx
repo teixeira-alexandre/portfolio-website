4faf6b288975eb346c0918cab142c5b093b20823
teixeira-alexandre <alexandre1997teixeira@gmail.com>
2024-04-23 05:21:47 -0300

    initializing nav bar

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div>
+        <Link href={"/"}>LOGO</Link>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
