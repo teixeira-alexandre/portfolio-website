687bd528855695ad9e75704d57e1487b6f532527
teixeira-alexandre <alexandre1997teixeira@gmail.com>
2024-04-25 04:59:54 -0300

    complete about section

---
+++ app/components/AboutSection.jsx
@@ -0,0 +1,107 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+    {
+        title: "Skills",
+        id: "skills",
+        content: (
+            <ul className="list-disc pl-2">
+                <li>JavaScript</li>
+                <li>TypeScript</li>
+                <li>React</li>
+                <li>Next.js</li>
+                <li>Node.js</li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Git</li>
+            </ul>
+        )
+    },
+    {
+        title: "Education",
+        id: "education",
+        content: (
+            <ul className="list-disc pl-2">
+                <li>Computer Science Degree</li>
+                <li>Web Development Bootcamp</li>
+            </ul>
+        )
+    },
+    {
+        title: "Certifications",
+        id: "certifications",
+        content: (
+            <ul className="list-disc pl-2">
+                <li>The Odin Project Fundations</li>
+                <li>FreeCodeCamp Responsive Web Design</li>
+                <li>FreeCodeCamp JavaScript Algorithms and Data Structures</li>
+            </ul>
+        )
+    }
+]
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image
+          src="/images/about-image.jpeg"
+          width={500}
+          height={500}
+          alt="About Image"
+        />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            I am a Front-End Web Developer with a passion for creating
+            interactive and responsive web applications. I have experience
+            working with JavaScript, TypeScript, React, Next.js, Node.js, HTML,
+            CSS and Git. I&apos;m a nice guy who likes to play videogames, watch
+            anime, and learn new skills. I&apos;m excited to work with other to
+            create wonderful applications.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
