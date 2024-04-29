de262ace018d9554fad5b110038cdd415b2521c4
teixeira-alexandre <alexandre1997teixeira@gmail.com>
2024-04-29 03:59:04 -0300

    include project placeholder

--- app/components/AchievementsSection.jsx
+++ app/components/AchievementsSection.jsx
@@ -1,36 +1,61 @@
+"use client";
 import React from "react";
+import dynamic from "next/dynamic";
+
+const AnimatedNumbers = dynamic(
+  () => {
+    return import("react-animated-numbers");
+  },
+  { ssr: false }
+);
 
 const achievementsList = [
   {
     metric: "Projects",
-    value: "100+",
+    value: "100",
+    postfix: "+",
   },
   {
     metric: "Users",
-    value: "1M+",
+    value: "100",
+    postfix: "~",
   },
   {
     metric: "Awards",
-    value: "10+",
+    value: "2",
   },
   {
     metric: "Years",
-    value: "2+",
+    value: "2",
   },
 ];
 
 const AchievementsSection = () => {
   return (
     <div className="py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-      <div className="border[##33353F] border rounded-md py-8 px17 flex flex-row items-center justify-between">
+      <div className="border[##33353F] border rounded-md py-8 px16 flex flex-row items-center justify-between">
         {achievementsList.map((achievementsList, index) => {
           return (
             <div
               key={index}
               className="flex flex-col items-center justify-center mx-4"
             >
-              <h2 className="text-white text-4xl font-bold">
-                {achievementsList.value}
+              <h2 className="text-white text-4xl font-bold flex flex-row">
+                {achievementsList.prefix}
+                <AnimatedNumbers
+                  includeComma
+                  animateToNumber={parseInt(achievementsList.value)}
+                  locale="en-US"
+                  className="text-white text-4xl font-bold"
+                  configs={(_, index) => {
+                    return {
+                    mass: 1,
+                    friction: 100,
+                    tensions: 140 * (index + 1),
+                    };
+                  }}
+                  />
+                  {achievementsList.postfix}
               </h2>
               <p className="text-[#ADB7BE] text-base">
                 {achievementsList.metric}
--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -1,66 +1,75 @@
 "use client";
-import React from 'react';
-import Image from 'next/image';
-import { TypeAnimation } from 'react-type-animation';
-
-
-    
+import React from "react";
+import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
+import { motion } from "framer-motion";
 
 const HeroSection = () => {
   return (
-    <section className='lg:py-16'>
-        <div className='grid grid-cols-1 sm:grid-cols-12'>
-          <div className='col-span-8 place-self-center text-center sm:text-left justify-self-start'>
-            <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold'>
-              <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
-                Hello, I&#39;m{" "}
-                </span>
-                <br></br>
-                <TypeAnimation
-                      sequence={[
-                        'Alexandre',
-                        1000,
-                        'Web Developer',
-                        1000,
-                        'Software Engineer',
-                        1000,
-                        'The guy you want',
-                        1000
-                      ]}
-                      wrapper="span"
-                      speed={50}
-                      style={{fontSize: '85px'}}
-                      repeat={Infinity} />    
-              </h1>
-            <p className='text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl'>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi, iste? Nisi quo neque libero repudiandae excepturi unde vel quisquam. Eius in rem fugiat ipsum laboriosam fuga quod eligendi odio voluptatum?
-            </p>
-            <div>
-              <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-primary-500 to-secondary-500 hover:bg-slate-200 text-white'>
-                Hire Me!
-              </button>
-              <button className='px-1 py-1 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3'>
-                <div className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-                  Download CV
-                  </div>
-              </button>
-
-            </div>
+    <section className="lg:py-16">
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1}}
+          transition={{ duration: 0.5 }}
+          className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
+        >
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
+              Hello, I&#39;m{" "}
+            </span>
+            <br></br>
+            <TypeAnimation
+              sequence={[
+                "Alexandre",
+                1000,
+                "Web Developer",
+                1000,
+                "Software Engineer",
+                1000,
+                "The guy you want",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              style={{ fontSize: "82px" }}
+              repeat={Infinity}
+            />
+          </h1>
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi,
+            iste? Nisi quo neque libero repudiandae excepturi unde vel quisquam.
+            Eius in rem fugiat ipsum laboriosam fuga quod eligendi odio
+            voluptatum?
+          </p>
+          <div>
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-primary-500 to-secondary-500 hover:bg-slate-200 text-white">
+              Hire Me!
+            </button>
+            <button className="px-1 py-1 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3">
+              <div className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                Download CV
+              </div>
+            </button>
           </div>
-          <div className='col-span-4 place-self-center mt-4 lg:mt-0'>
-            <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
-              <Image
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1}}
+          transition={{ duration: 0.5 }} className="col-span-4 place-self-center mt-4 lg:mt-0">
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+            <Image
               src="/images/memoji.png"
               alt="hero image"
-              className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={300}
               height={300}
-              />
-            </div>
+            />
           </div>
-        </div>
+        </motion.div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default HeroSection
+export default HeroSection;
