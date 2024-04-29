7f3ba5c200debe320e6d100d0073c0231fa5d068
teixeira-alexandre <alexandre1997teixeira@gmail.com>
2024-04-29 05:39:35 -0300

    fix sections and add name

--- app/components/EmailSection.tsx
+++ app/components/EmailSection.tsx
@@ -22,7 +22,7 @@ const EmailSection: FC = () => {
   }
 
   return (
-    <section className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative">
+    <section id="contact" className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative">
       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
       <div className="z-10">
         <h5 className="text-xl font-bold text-white my-2">
@@ -35,10 +35,10 @@ const EmailSection: FC = () => {
           try my best to get back to you!
         </p>
         <div className="socials flex flex-row gap-2">
-          <Link href="github.com/teixeira-alexandre">
+          <Link href="https://www.github.com/teixeira-alexandre">
             <Image src={GithubIcon} alt="Github Icon" width={50} height={50} />
           </Link>
-          <Link href="linkedin.com/in/alexandre-teixeira-a869622aa/">
+          <Link href="https://www.linkedin.com/in/alexandre-teixeira-a869622aa/">
             <Image
               src={LinkedInIcon}
               alt="LinkedIn Icon"
