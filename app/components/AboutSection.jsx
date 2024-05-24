5fc27fba44cccec1e0f6408026acb8a4cc735df8
teixeira-alexandre <alexandre1997teixeira@gmail.com>
2024-05-24 02:19:11 -0300

    fix sonarcloud performance issues

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -46,7 +46,7 @@ const TAB_DATA = [
 
 const AboutSection = () => {
   const [tab, setTab] = useState("skills");
-  const [isPending, startTransition] = useTransition();
+  const [, startTransition] = useTransition();
 
   const handleTabChange = (id) => {
     startTransition(() => {
